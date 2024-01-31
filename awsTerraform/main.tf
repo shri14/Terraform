79ad5fad15df3f62fda945ebb6791ec5426bab85
Devopsbuddy <98806710+shri14@users.noreply.github.com>
2024-01-31 14:09:07 +0530

    adding resource

--- awsTerraform/main.tf
+++ awsTerraform/main.tf
@@ -8,5 +8,26 @@ terraform {
   }
 }
 provider "aws" {
-  region = "u"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-0c94855ba937ecf"
+  instance_type = "t2.micro"
+  count         = 2
+  tags = {
+    Name = "terraform-instance_example"
+  }
+}
+
+output "public_ips" {
+  value = aws_instance.instance[*].public_ip
+}
+
+resource "aws_s3_bucket" "aws2024" {
+  bucket = "my-tf-test-bucket-2024"
+  tags = {
+    name        = "shrikar_bucket"
+    Environment = "dev"
+  }
 }
